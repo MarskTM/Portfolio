5cf72718a583dfba9356144dc857ed4876dacc56
MarskTM <manhtokim@gmail.com>
2025-03-16 21:24:58 +0700

    update: layout my blogs

--- frontend/src/components/blog/page.js
+++ frontend/src/components/blog/page.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Link from "next/link";
-import { ArrowRightIcon } from "@heroicons/react/24/solid";
+import { ArrowRightIcon, ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
 import { BlogCard } from "@components/card";
 import Pagination from "@components/pagination/page";
 
 const blogs = [
     {
-        title: 'Creating an Intuitive User Interface (UI) for Your SaaS Product',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...',
+        title: 'Creating an Intuitive User Interface (UI) for Your SaaS Product Creating an Intuitive User Interface (UI) for Your SaaS Product Creating an Intuitive User Interface (UI) for Your SaaS Product',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqui Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip',
         author: 'John Doe',
         time: '10 mins ago',
         category: 'Frontend',
@@ -37,6 +37,74 @@ const blogs = [
         category: 'Node.js',
         image: '/images/project_img.png', // Đường dẫn tới hình ảnh của bạn
     },
+    {
+        title: 'Building Scalable APIs with Node.js',
+        description: 'Discover best practices for building scalable and maintainable APIs using Node.js and Express.',
+        author: 'Emily White',
+        time: '25 mins ago',
+        category: 'Node.js',
+        image: '/images/project_img.png', // Đường dẫn tới hình ảnh của bạn
+    },
+];
+
+const blogs2 = [
+    {
+        title: 'Creating an Intuitive User Interface (UI) for Your SaaS Product Creating an Intuitive User Interface (UI) for Your SaaS Product Creating an Intuitive User Interface (UI) for Your SaaS Product',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...',
+        author: 'John Doe',
+        time: '10 mins ago',
+        category: 'Frontend',
+        image: '/images/project_img.png', // Đường dẫn tới hình ảnh của bạn
+    },
+    {
+        title: 'Mastering State Management in React',
+        description: 'Explore different state management solutions in React and learn how to choose the right one for your project.',
+        author: 'Jane Smith',
+        time: '15 mins ago',
+        category: 'React',
+        image: '/images/project_img.png', // Đường dẫn tới hình ảnh của bạn
+    },
+    {
+        title: 'Introduction to Next.js Routing',
+        description: 'Learn how to create dynamic routes in Next.js and understand the different routing strategies.',
+        author: 'David Lee',
+        time: '20 mins ago',
+        category: 'Next.js',
+        image: '/images/project_img.png', // Đường dẫn tới hình ảnh của bạn
+    },
+];
+
+const categories = [
+    { name: 'Frontend', count: 5 },
+    { name: 'Backend', count: 4 },
+    { name: 'AI', count: 1 },
+    { name: 'Other', count: 3 },
+];
+
+// components/Tags.js
+const tagsData = [
+    { name: 'algorithm', size: 'text-2xl' },
+    { name: 'azure', size: 'text-sm' },
+    { name: 'blog', size: 'text-base' },
+    { name: 'C#', size: 'text-lg' },
+    { name: 'c#.net', size: 'text-xl' },
+    { name: 'c# hay ho', size: 'text-base' },
+    { name: 'code', size: 'text-sm' },
+    { name: 'coder', size: 'text-xs' },
+    { name: 'coding', size: 'text-base' },
+    { name: 'cv', size: 'text-lg' },
+    { name: 'công nghệ', size: 'text-3xl' },
+    { name: 'database', size: 'text-sm' },
+    { name: 'developer', size: 'text-base' },
+    { name: 'dự án', size: 'text-lg' },
+    { name: 'facebook', size: 'text-xl' },
+    { name: 'framework', size: 'text-sm' },
+    { name: 'front-end', size: 'text-xs' },
+    { name: 'javascript', size: 'text-base' },
+    { name: 'js', size: 'text-lg' },
+    { name: 'junior', size: 'text-2xl' },
+    { name: 'kinh nghiệm', size: 'text-sm' },
+    { name: 'lập trình', size: 'text-xs' }
 ];
 
 export default function Blog() {
@@ -59,19 +127,15 @@ export default function Blog() {
 
 
                 {/* Danh sách bài viết */}
-                <div className="flex flex-row gap-8 mt-6">
+                <div className="flex flex-col lg:flex-row gap-14 mt-6">
                     <div className="flex-2/3 container mx-auto">
-                        <div className="flex items-center justify-between">
-                            <h1 className="text-left text-4xl font-bold mb-4">Keep Reading</h1>
-                            <Link href="/" className="text-vase font-bold">
-                                <span className="text-gray-500">See More </span>
-                            </Link>
-                        </div>
+
                         <div className="grid grid-cols-1 gap-4">
                             {blogs.map((blog, index) => (
                                 <Link href="/" key={index}>
                                     <BlogCard
                                         title={blog.title}
+                                        titleSize={"text-2xl"}
                                         description={blog.description}
                                         author={blog.author}
                                         time={blog.time}
@@ -82,12 +146,60 @@ export default function Blog() {
 
                             ))}
                         </div>
-                        <Pagination showControls currentPage={currentPage} totalPages={5} onPageChange={(page) => setCurrentPage(page)} />
+                        <div className="flex items-center justify-end mt-10">
+                            {/* <h1 className="text-left text-4xl font-bold mb-4">Keep Reading</h1> */}
+                            <Link href="/" className="text-vase font-bold flex flex-row justify-center items-center">
+                                <span className="text-gray-500 hover:text-blue-400">See more</span><ChevronDoubleRightIcon className="size-4 text-blue-900" />
+                            </Link>
+                        </div>
+                        {/* <Pagination showControls currentPage={currentPage} totalPages={5} onPageChange={(page) => setCurrentPage(page)} /> */}
                     </div>
 
+                    {/* Sub menu */}
                     <div className="flex-1/3 container mx-auto p-4 bg-gray-50 rounded-l-2xl shadow-xl">
-                        <h1 className="text-center text-2xl font-bold mb-4">Popular Post</h1>
-                        <div className="flex flex-wrap gap-2"></div>
+
+                        {/* Popular Post */}
+                        <div className="flex flex-wrap gap-2 md:hidden lg:block">
+                            <h1 className="text-center text-2xl font-bold my-7">Popular Post</h1>
+                            <div className="grid grid-cols-1 grid-rows-3 gap-4 overflow-hidden">
+                                {blogs2.map((blog, index) => (
+                                    <Link href="/" key={index}>
+                                        <BlogCard
+                                            title={blog.title}
+                                            author={blog.author}
+                                            time={blog.time}
+                                            image={blog.image}
+                                        />
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+
+                        {/* Categories */}
+                        <h1 className="text-center text-2xl font-bold my-7">Categories</h1>
+                        <div className=" p-4 mx-auto space-y-2">
+                            {categories.map((category) => (
+                                <Link key={category.name} href="/" className="text-base p-2 flex justify-between items-center border-b border-gray-200 hover:bg-gray-200 transition">
+                                    <span className="text-gray-700">{category.name}</span>
+                                    <span className="text-gray-500 text-sm">({category.count})</span>
+                                </Link>
+                            ))}
+                        </div>
+
+                        {/* tags */}
+                        <h1 className="text-center text-2xl font-bold mt-7">Tags</h1>
+                        <div className="p-4 mx-auto lg:w-full md:w-3/4">
+                            <div className="flex flex-wrap items-center justify-center gap-2">
+                                {tagsData.map((tag, index) => (
+                                    <span
+                                        key={index}
+                                        className={`py-1 rounded-full font-medium text-green-400/80 hover:text-green-600 hover:scale-3d translate-0.5 ${tag.size}`}
+                                    >
+                                        {tag.name}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/card/blogItem.js
+++ frontend/src/components/card/blogItem.js
@@ -2,11 +2,11 @@
 import { Image } from "@heroui/react";
 import { UserCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
 
-export default function BlogCard({ title, description, author, time, category, image }) {
+export default function BlogCard({ title, description, author, time, category, image, titleSize }) {
     return (
-        <div className="flex items-start space-x-4 p-4 bg-white rounded-lg transition-all duration-500">
+        <div className="flex items-start space-x-4 p-4 rounded-lg transition-all duration-500">
             {/* Hình ảnh */}
-            <div className="w-48 h-36 flex-shrink-0 rounded-2xl overflow-hidden">
+            <div className="w-1/4 h-max max-h-36 flex-shrink-0 rounded-2xl overflow-hidden">
                 {image &&
                     <Image
                         src={image}
@@ -17,28 +17,24 @@ export default function BlogCard({ title, description, author, time, category, i
             </div>
 
             {/* Nội dung */}
-            <div className="flex-1">
+            <div className="flex-1 w-3/4 items-stretch ">
                 {/* Danh mục */}
-                <div className="flex flex-row space-x-2">
-
-                    <div className="w-max text-sm text-gray-600 px-1 py-1 rounded-4xl shadow-sm">
+                {
+                    category &&
+                    <div className="w-max text-sm text-gray-600 px-1 py-1 rounded-4xl shadow-md">
                         <span className="px-2 text-xs">
                             {category}
                         </span>
                     </div>
-
-                </div>
-
-
-
+                }
 
                 {/* Tiêu đề */}
-                <h2 className="text-xl text-left font-semibold mt-2 hover:text-blue-500 cursor-pointer">
+                <h2 className={`max-w-full h-1/2 text-left font-semibold mt-2 hover:text-blue-500 cursor-pointer overflow-y-hidden truncate ${titleSize ? titleSize : "text-sm"}`}>
                     {title}
                 </h2>
 
                 {/* Mô tả */}
-                <p className="text-gray-600 text-base text-left mt-1 line-clamp-2">{description}</p>
+                <p className="max-w-full text-gray-600 text-base text-left mt-1 line-clamp-2 overflow-y-hidden text-ellipsis ">{description}</p>
 
                 {/* Tác giả & Thời gian */}
                 <div className="flex items-center text-gray-500 text-xs mt-3 space-x-2">
